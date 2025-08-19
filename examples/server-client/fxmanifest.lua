ac8f17683a7bb0bb52225a33deaad2271da76f4f
XenoS-ITA <arcobalenosixsiegea@gmail.com>
2025-08-19 18:41:04 +0200

    fix(examples/server-client): remove prints and fix manifest

--- examples/server-client/fxmanifest.lua
+++ examples/server-client/fxmanifest.lua
@@ -5,7 +5,6 @@ game 'gta5'
 client_scripts {
     "@utility_lib/client/native.lua",
     "build/client/objectify.lua",
-    "build/client/functions/*.lua",
     "build/client/objects/**.lua",
     "build/client/main.lua",
 }
@@ -13,8 +12,7 @@ client_scripts {
 server_scripts {
     "@utility_lib/server/native.lua",
     "build/server/objectify.lua",
-    "build/server/functions/*.lua",
-    "build/server/objects/*.lua",
+    "build/server/objects/**.lua",
     "build/server/main.lua",
 }
 
